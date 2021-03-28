701d99e2da0fc399f4428f682a2bfa2bb369332c
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-27 19:57:04 -0700

    Section 4: Trapping Rainwater - Optimized

--- Arrays/trappingRainwater.js
+++ Arrays/trappingRainwater.js
@@ -23,27 +23,70 @@ The amount of rainwater we can trap at any given index is the min of either wall
 Time:  O(n^2)
 Space: O(1)
 */
+// const trappingRainwater = (heights) => {
+//   let result = 0;
+
+//   for (let i = 1; i < heights.length; i++) {
+//     // const leftWall = Math.max(...heights.slice(0, i + 1)); // Math.max and slice are both O(n)
+//     // const rightWall = Math.max(...heights.slice(i));
+//     const leftWall = findWall(heights, i, true);
+//     const rightWall = findWall(heights, i, false);
+//     result += Math.min(leftWall, rightWall) - heights[i];
+//   }
+
+//   return result;
+// };
+
+// const findWall = (heights, index, leftWall) => {
+//   let start = leftWall ? 0 : index;
+//   let end = leftWall ? index + 1 : heights.length;
+//   let result = 0;
+
+//   for (start; start < end; start++) {
+//     result = Math.max(result, heights[start]);
+//   }
+
+//   return result;
+// };
+
+/*
+Optimizing
+
+Two Pointer:
+- start at both ends of the input
+- Figure out which pointer has the lesser value
+- Is that pointer >= to the max of that side?
+    - Yes; update max for that side
+    - No; get water and update result
+- Move pointer inward
+
+Time:  O(n)
+Space: O(1)
+*/
 const trappingRainwater = (heights) => {
+  let leftWall = 0;
+  let rightWall = 0;
+  let leftPointer = 0;
+  let rightPointer = heights.length - 1;
   let result = 0;
 
-  for (let i = 1; i < heights.length; i++) {
-    // const leftWall = Math.max(...heights.slice(0, i + 1)); // Math.max and slice are both O(n)
-    // const rightWall = Math.max(...heights.slice(i));
-    const leftWall = findWall(heights, i, true);
-    const rightWall = findWall(heights, i, false);
-    result += Math.min(leftWall, rightWall) - heights[i];
-  }
-
-  return result;
-};
-
-const findWall = (heights, index, leftWall) => {
-  let start = leftWall ? 0 : index;
-  let end = leftWall ? index + 1 : heights.length;
-  let result = 0;
-
-  for (start; start < end; start++) {
-    result = Math.max(result, heights[start]);
+  while (leftPointer < rightPointer) {
+    const leftSmaller = heights[leftPointer] < heights[rightPointer];
+    if (leftSmaller) {
+      if (heights[leftPointer] >= leftWall) {
+        leftWall = heights[leftPointer];
+      } else {
+        result += leftWall - heights[leftPointer];
+      }
+      leftPointer++;
+    } else {
+      if (heights[rightPointer] >= rightWall) {
+        rightWall = heights[rightPointer];
+      } else {
+        result += rightWall - heights[rightPointer];
+      }
+      rightPointer--;
+    }
   }
 
   return result;
