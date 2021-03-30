37649ddeb095d0778e84dc8bbd2e1f1d690358e6
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-29 19:51:50 -0700

    Section 6: Longest Substring Without Repeating Characters - Brute Force

---
+++ Strings/longestSubtringWithoutRepeatingChars.js
@@ -0,0 +1,86 @@
+/*
+Given a string, find the length of the longest substring without repeating characters
+
+Constraints:
+- Is the substring contiguous?
+  - Yes, look for a substring not a subsquence
+    - substring vs. subsequence
+      - contiguous: chars are sequential and do not have breaks
+      - ex. SUBSTRING of 'abcbbd' could be 'abc'
+      - ex. SUBSEQUENCE of 'abcbbd' could be 'abc__d'
+- Does case sensitivity matter?
+  - No, assume all chars are lowercase
+*/
+
+/**
+ *
+ * @param {String} s
+ * @returns length of longest substring of input "s" without repeating characters
+ */
+
+/*
+Brute Force
+
+Find all combinations of substrings without repeating chars
+Nested for loop
+- for every char as index i, create a hash
+  - from that char as index j, iterate until end of input or if we find a repeating char
+    - if the char is not already in the hash
+      - add it to the hash, and determine if j - i + 1 > result
+    - else, break nested loop
+Return result
+
+Time:  O(n^2) where n = s.length
+Space: O(1)
+
+*/
+const longestSubstringWithoutRepeatingChars = (s) => {
+	let result = 0;
+
+	for (let i = 0; i < s.length; i++) {
+		const hash = {};
+		for (let j = i; j < s.length; j++) {
+			if (hash[s[j]]) break;
+			hash[s[j]] = true;
+			result = Math.max(result, j - i + 1);
+		}
+	}
+
+	return result;
+};
+
+// Example 1
+let s = 'abccabb';
+let Result = longestSubstringWithoutRepeatingChars(s);
+console.log({ Inputs: { s }, Result, Expected: 3 });
+console.log('------------');
+
+// Example 2
+s = 'cccccc';
+Result = longestSubstringWithoutRepeatingChars(s);
+console.log({ Inputs: { s }, Result, Expected: 1 });
+console.log('------------');
+
+// Example 3
+s = '';
+Result = longestSubstringWithoutRepeatingChars(s);
+console.log({ Inputs: { s }, Result, Expected: 0 });
+console.log('------------');
+
+// Example 4
+s = 'abcbda';
+Result = longestSubstringWithoutRepeatingChars(s);
+console.log({ Inputs: { s }, Result, Expected: 4 });
+console.log('------------');
+
+// // Example 5
+// s = 'Ab#z';
+// Result = longestSubstringWithoutRepeatingChars(s);
+// console.log({ Inputs: { s }, Result, Expected: false });
+// console.log('------------');
+
+// // Example 5
+// s = 'abc#d';
+// Result = longestSubstringWithoutRepeatingChars(s);
+// console.log({ Inputs: { s }, Result, Expected: true });
+// console.log('------------');
