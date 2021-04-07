0c4d06c07ee71b6fe246b28283f1cda098536faa
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-07 15:35:20 -0700

    Section 45: Insertion Sort - Udemy solution

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -5,20 +5,46 @@
 
 /*
 Useful for almost sorted lists
+For every element, find where it belongs and insert it
+In the example below, we basically bubble down the number in its correct position
+- The left side of the array will slowly get sorted and we can stop when we find a position such that for elements at index i, array[i - 1] < array[i] < array[i + 1]
 
 Time:  O(n^2) => can be O(n) for sorted lists
 Space: O(1)
 */
+// const insertionSort = (array) => {
+//   for (let i = 1; i < array.length; i++) {
+//     let j = i;
+//     while (j >= 0) {
+//       if (array[j] < array[j - 1]) {
+//         let swap = array[j];
+//         array[j] = array[j - 1];
+//         array[j - 1] = swap;
+//         j--;
+//       } else break;
+//     }
+//   }
+// };
+
+// Udemy solution
 const insertionSort = (array) => {
-  for (let i = 1; i < array.length; i++) {
-    let j = i;
-    while (j >= 0) {
-      if (array[j] < array[j - 1]) {
-        let swap = array[j];
-        array[j] = array[j - 1];
-        array[j - 1] = swap;
-        j--;
-      } else break;
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      // Move number to the first position
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // Only sort the number small than the number to the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+      if (array[i] < array[i - 1]) {
+        // Find the index to insert
+        for (let j = 1; j < i; j++) {
+          if (array[i] >= array[j - 1] && array[i] < array[j]) {
+            // Insert
+            array.splice(j, 0, array.splice(i, 1)[0]);
+          }
+        }
+      }
     }
   }
 };
