bb69c37215f35388a3837c8c3eccef1a8d1374d6
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-07 15:29:50 -0700

    Section 45: Insertion Sort

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * @param {Number[]} array
+ */
+
+/*
+Useful for almost sorted lists
+
+Time:  O(n^2) => can be O(n) for sorted lists
+Space: O(1)
+*/
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j >= 0) {
+      if (array[j] < array[j - 1]) {
+        let swap = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = swap;
+        j--;
+      } else break;
+    }
+  }
+};
+
+// Test 1
+let array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+console.log("Before --> ", array);
+insertionSort(array);
+console.log({
+  Result: array,
+  Expected: [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283],
+});
+console.log("------------");
+
+// Test 2
+array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+console.log("Before --> ", array);
+insertionSort(array);
+console.log({
+  Result: array,
+  Expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+});
+console.log("------------");
+
+// Test 3
+array = [-1, 5, 3, 14, 6, 56, 93, -123, 69, -420];
+console.log("Before --> ", array);
+insertionSort(array);
+console.log({
+  Result: array,
+  Expected: [-420, -123, -1, 3, 5, 6, 14, 56, 69, 93],
+});
+console.log("------------");
