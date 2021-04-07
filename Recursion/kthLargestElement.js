6e2f018a6862d34640fc0bc91c225f2e9b98a342
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-06 22:53:26 -0700

    Section 15: Kth Largest Element - My Solution

---
+++ Recursion/kthLargestElement.js
@@ -0,0 +1,74 @@
+/*
+
+Given an unsorted array, return the kth largest element. It is the kth largest element in sorted order, not the kth distinct element.
+
+Constraints:
+- Can we get an array where k is larger than the array length?
+    - No, assume an answer is always available.
+*/
+
+/**
+ *
+ * @param {Number[]} nums unsorted array of numbers
+ * @param {Number} k
+ * @returns the kth largest element
+ */
+
+/*
+My solution
+
+Sort the 
+
+Time:  O(n*log(n)) b/c of sorting
+Space: O(k) b/c of k recursive calls
+*/
+const kthLargestElement = (nums, k) => {
+  nums.sort((a, b) => a - b);
+  if (k === 1) return nums[nums.length - 1];
+  return kthLargestElement(nums.slice(0, nums.length - 1), k - 1);
+};
+
+/*
+  Optimizing
+  
+  Time:  O()
+  Space: O()
+  */
+// const kthLargestElement = (nums, k) => {
+//     return false
+// }
+
+// Test 1
+let nums = [5, 3, 1, 6, 4, 2];
+let k = 2;
+let Result = kthLargestElement(nums, k);
+console.log({ Inputs: { nums, k }, Result, Expected: 5 });
+console.log("------------");
+
+// Test 2
+nums = [2, 3, 1, 2, 4, 2];
+k = 4;
+Result = kthLargestElement(nums, k);
+console.log({ Inputs: { nums, k }, Result, Expected: 2 });
+console.log("------------");
+
+// Test 3
+nums = [3];
+k = 1;
+Result = kthLargestElement(nums, k);
+console.log({ Inputs: { nums, k }, Result, Expected: 3 });
+console.log("------------");
+
+// // Test 4
+// nums = ;
+// k = ;
+// Result = kthLargestElement(nums, k);
+// console.log({ Inputs: { nums, k }, Result, Expected: 0 });
+// console.log("------------");
+
+// // Test 5
+// nums = ;
+// k = ;
+// Result = kthLargestElement(nums, k);
+// console.log({ Inputs: { nums, k }, Result, Expected: 0 });
+// console.log("------------");
