2ec9ab17b3964bf74dcb894245bca00d5f7adf0a
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-07 13:42:53 -0700

    Section 45: Bubble Sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -24,30 +24,30 @@ const bubbleSort = (array) => {
 
 // Test 1
 let array = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+console.log("Before --> ", array);
 bubbleSort(array);
 console.log({
-  Inputs: { array },
-  array,
+  Result: array,
   Expected: [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283],
 });
 console.log("------------");
 
 // Test 2
 array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+console.log("Before --> ", array);
 bubbleSort(array);
 console.log({
-  Inputs: { array },
-  array,
+  Result: array,
   Expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 });
 console.log("------------");
 
 // Test 3
 array = [-1, 5, 3, 14, 6, 56, 93, -123, 69, -420];
+console.log("Before --> ", array);
 bubbleSort(array);
 console.log({
-  Inputs: { array },
-  array,
+  Result: array,
   Expected: [-420, -123, -1, 3, 5, 6, 14, 56, 69, 93],
 });
 console.log("------------");
