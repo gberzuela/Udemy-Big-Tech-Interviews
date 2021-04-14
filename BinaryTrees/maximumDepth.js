58c7bb4f1d3194d7fa229189d61559b2e163f4dc
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-14 14:46:43 -0700

    Section 17: Maximum Depth of Binary Tree - Udemy solution

--- BinaryTrees/maximumDepth.js
+++ BinaryTrees/maximumDepth.js
@@ -37,20 +37,45 @@ Return the max between result + left height and result + right height
 
 Time:  O(n) where n = the number of nodes in the Binary Tree
 Space: O(h) where h = the maximum height of the Binary Tree
+       O(log(n))  h = log(n)
 */
-const maximumDepth = (root) => {
-  if (!root) return 0;
+// const maximumDepth = (root) => {
+//   if (!root) return 0;
 
-  const result = 1;
-  let leftHeight = 0;
-  let rightHeight = 0;
+//   const result = 1;
+//   let leftHeight = 0;
+//   let rightHeight = 0;
 
-  if (root.left) leftHeight = maximumDepth(root.left);
-  if (root.right) rightHeight = maximumDepth(root.right);
+//   if (root.left) leftHeight = maximumDepth(root.left);
+//   if (root.right) rightHeight = maximumDepth(root.right);
 
-  return Math.max(result + leftHeight, result + rightHeight);
+//   return Math.max(result + leftHeight, result + rightHeight);
+// };
+
+/*
+Udemy solution:
+Keep track of currentDepth in the function definition (0 if none is passed)
+If input, root, is null, return 0
+Increment currentDepth
+Return the max between the depth of the left subtree and the depth of the right subtree
+
+Time:  O(n) where n = number of nodes in Binary Tree
+Space: O(h) where h = maximum height of the Binary Tree 
+       O(log(n))  h = log(n)
+*/
+const maximumDepth = (root, currentDepth = 0) => {
+  if (!root) return currentDepth;
+
+  currentDepth++;
+
+  return Math.max(
+    maximumDepth(root.left, currentDepth),
+    maximumDepth(root.right, currentDepth)
+  );
 };
 
+// Prints a Binary Tree through BFS
+// Indicates the nodes value as well as what side it's on
 const printBinaryTree = (root) => {
   let stack = [[root, "root"]];
 
@@ -62,19 +87,20 @@ const printBinaryTree = (root) => {
   }
 };
 
-/*
-Optimizing
-
-Time:  O()
-Space: O()
-*/
-// const maximumDepth = (root) => {
-//     return false
-// }
-
 // Test 1
 let root = new BinaryTree(5);
 [4, 2, 3, 4, 6].forEach((val) => root.insert(val));
+/*
+    5
+   / \
+  4   6
+ /
+2
+ \
+  3
+   \
+    4
+*/
 let Result = maximumDepth(root);
 console.log({ Inputs: { root }, Result, Expected: 5 });
 console.log("------------");
@@ -88,6 +114,19 @@ console.log("------------");
 // Test 3
 root = new BinaryTree(1);
 [2, 3, 4, 5, 6].forEach((val) => root.insert(val));
+/*
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+*/
 Result = maximumDepth(root);
 console.log({ Inputs: { root }, Result, Expected: 6 });
 console.log("------------");
