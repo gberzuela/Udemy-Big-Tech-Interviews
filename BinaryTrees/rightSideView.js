e104aefded0e70994741a8ea52894fa019108557
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-17 09:46:37 -0700

    Section 19: Right Side View of Tree - Udemy BFS solution

--- BinaryTrees/rightSideView.js
+++ BinaryTrees/rightSideView.js
@@ -1,4 +1,4 @@
-const BinaryTree = require('../utils/BinaryTree');
+const BinaryTree = require("../utils/BinaryTree");
 
 /*
 Given a binary tree, imagine you're standing to the right of the tree. Return an array of the values of the nodes you can see ordered from top to bottom.
@@ -27,31 +27,58 @@ My solution
 Pre-process the binary tree by levels
 Basically solve for level order and filter each level for the last/right most node
 
+Time:  O(2n) => O(n) where n = number of nodes in BinaryTree
+Space: O(n)
+*/
+// const rightSideView = (root) => {
+//   if (!root) return [];
+
+//   const result = [];
+//   const queue = [[root, 0]];
+
+//   // Process level order
+//   while (queue.length) {
+//     const [node, level] = queue.shift();
+//     if (result[level]) result[level].push(node);
+//     else result[level] = [node];
+
+//     if (node.left) queue.push([node.left, level + 1]);
+//     if (node.right) queue.push([node.right, level + 1]);
+//   }
+
+//   // Reduce our level order to only have the last node of every level
+//   return result.reduce((accu, next) => {
+//     accu.push(next[next.length - 1].value);
+//     return accu;
+//   }, []);
+// };
+
+/*
+Udemy BFS solution
+Optimized level order approach
+
 Time:  O(n) where n = number of nodes in BinaryTree
 Space: O(n)
 */
 
 const rightSideView = (root) => {
-	if (!root) return [];
+  if (!root) return [];
 
-	const result = [];
-	const queue = [[root, 0]];
+  const result = [];
+  const queue = [root];
 
-	// Process level order
-	while (queue.length) {
-		const [node, level] = queue.shift();
-		if (result[level]) result[level].push(node);
-		else result[level] = [node];
+  while (queue.length) {
+    let current;
+    const length = queue.length;
+    for (let i = 0; i < length; i++) {
+      current = queue.shift();
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    result.push(current.value);
+  }
 
-		if (node.left) queue.push([node.left, level + 1]);
-		if (node.right) queue.push([node.right, level + 1]);
-	}
-
-	// Reduce our level order to only have the last node of every level
-	return result.reduce((accu, next) => {
-		accu.push(next[next.length - 1].value);
-		return accu;
-	}, []);
+  return result;
 };
 
 // Test 1 - Example from above
