9df16f704e34b3d595d084b48e1bbe72eb4e2d93
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-17 09:58:45 -0700

    ADD: template

---
+++ template.js
@@ -0,0 +1,67 @@
+/*
+
+Constraints:
+- 
+*/
+
+/**
+ *
+ * @param {*} param1
+ * @param {*} param2
+ * @returns
+ */
+
+/*
+Brute Force
+
+Time:  O()
+Space: O()
+*/
+const functionName = (param1, param2) => {
+  return false;
+};
+
+/*
+Optimizing
+
+Time:  O()
+Space: O()
+*/
+// const functionName = (param1, param2) => {
+//     return false
+// }
+
+// Test 1
+let param1 = "";
+let param2 = "";
+let Result = functionName(param1, param2);
+console.log({ Inputs: { param1, param2 }, Result, Expected: 0 });
+console.log("------------");
+
+// Test 2
+param1 = "";
+param2 = "";
+Result = functionName(param1, param2);
+console.log({ Inputs: { param1, param2 }, Result, Expected: 0 });
+console.log("------------");
+
+// Test 3
+param1 = "";
+param2 = "";
+Result = functionName(param1, param2);
+console.log({ Inputs: { param1, param2 }, Result, Expected: 0 });
+console.log("------------");
+
+// Test 4
+param1 = "";
+param2 = "";
+Result = functionName(param1, param2);
+console.log({ Inputs: { param1, param2 }, Result, Expected: 0 });
+console.log("------------");
+
+// Test 5
+param1 = "";
+param2 = "";
+Result = functionName(param1, param2);
+console.log({ Inputs: { param1, param2 }, Result, Expected: 0 });
+console.log("------------");
